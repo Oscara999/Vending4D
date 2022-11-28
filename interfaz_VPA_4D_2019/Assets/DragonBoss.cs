ffa28a5392e0e123630520d07825c7df94ffba05
OscarAlarcon99 <65203702+OscarAlarcon99@users.noreply.github.com>
2022-11-28 13:17:59 -0500

    edit game

---
+++ interfaz_VPA_4D_2019/Assets/DragonBoss.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonBoss : Singleton<DragonBoss>
+{
+    [SerializeField]
+    State currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
