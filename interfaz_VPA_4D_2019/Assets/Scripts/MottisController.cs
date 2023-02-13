fabf41b677e210a79c88c2b6ec6a2a7b123cc0d1
OscarAlarcon99 <65203702+OscarAlarcon99@users.noreply.github.com>
2023-02-13 16:55:03 -0500

    edit first cinematic

--- interfaz_VPA_4D_2019/Assets/Scripts/MottisController.cs
+++ interfaz_VPA_4D_2019/Assets/Scripts/MottisController.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class MottisController : MonoBehaviour
 {
+    [SerializeField]Transform camera;
     Animator anim;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();    
     }
 
+    private void Update()
+    {
+    }
+
     public void SetSpeakingBool(bool state)
     {
+        Debug.Log("Speaking " + state);
         anim.SetBool("IsTalking", state);
     }
+
+    public void SetMovingBool(bool state)
+    {
+        anim.SetBool("IsMoving", state);
+    }
+
     public void SetIndex(int index)
     {
         anim.SetInteger("Index", index);
