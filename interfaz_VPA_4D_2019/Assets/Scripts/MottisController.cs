b08875c1c4ee78d9f320c8fc3b2463e81a268375
OscarAlarcon99 <65203702+OscarAlarcon99@users.noreply.github.com>
2023-02-27 22:08:53 -0500

    edit first expressions

--- interfaz_VPA_4D_2019/Assets/Scripts/MottisController.cs
+++ interfaz_VPA_4D_2019/Assets/Scripts/MottisController.cs
@@ -5,36 +5,128 @@ using UnityEngine;
 public class MottisController : MonoBehaviour
 {
     [SerializeField]Transform camera;
-    Animator anim;
+    Animator bodyAnim;
+    [SerializeField] Animator tailAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();    
+        bodyAnim = GetComponent<Animator>();
     }
 
-    private void Update()
+    public void Call()
     {
+        StartCoroutine(StartAngry());
+    }
+
+
+    private IEnumerator StartAngry()
+    {
+        yield return new WaitForSeconds(2f);
+        bodyAnim.SetTrigger("Angry");
+        bodyAnim.SetBool("IsTalking", true);
+        bodyAnim.SetBool("IsStress", true);
+        tailAnim.SetBool("Angry", true);
+
+        yield return new WaitUntil(() => bodyAnim.GetCurrentAnimatorStateInfo(1).IsName("Molesto 1") &&
+            bodyAnim.GetCurrentAnimatorStateInfo(1).normalizedTime < 1.0f);
+
+        bodyAnim.SetTrigger("Angry");
+
+        yield return new WaitUntil(() => bodyAnim.GetCurrentAnimatorStateInfo(1).IsName("Molesto 1") &&
+    bodyAnim.GetCurrentAnimatorStateInfo(1).normalizedTime < 1.0f);
+
+        bodyAnim.SetBool("IsStress", false);
+        bodyAnim.SetBool("IsTalking", false);
+        tailAnim.SetBool("Angry", false);
+
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(StartHappy());
+
+
+    }
+
+    private IEnumerator StartHappy()
+    {
+        yield return new WaitForSeconds(2f);
+        bodyAnim.SetBool("IsHappy",true);
+        bodyAnim.SetBool("IsTalking", true);
+        tailAnim.SetBool("Happy", true);
+
+        yield return new WaitUntil(() => bodyAnim.GetCurrentAnimatorStateInfo(1).IsName("Molesto 1") &&
+            bodyAnim.GetCurrentAnimatorStateInfo(1).normalizedTime < 1.0f);
+
+        bodyAnim.SetTrigger("Angry");
+
+        yield return new WaitUntil(() => bodyAnim.GetCurrentAnimatorStateInfo(1).IsName("Molesto 1") &&
+    bodyAnim.GetCurrentAnimatorStateInfo(1).normalizedTime < 1.0f);
+
+        bodyAnim.SetBool("IsStress", false);
+        bodyAnim.SetBool("IsTalking", false);
+        tailAnim.SetBool("Angry", false);
+
+
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(StartHappy());
+
+    }
+
+
+    private IEnumerator StarSad()
+    {
+        yield return new WaitForSeconds(2f);
+        bodyAnim.SetTrigger("Angry");
+        bodyAnim.SetBool("IsTalking", true);
+        bodyAnim.SetBool("IsStress", true);
+        tailAnim.SetBool("Angry", true);
+
+        yield return new WaitUntil(() => bodyAnim.GetCurrentAnimatorStateInfo(1).IsName("Molesto 1") &&
+            bodyAnim.GetCurrentAnimatorStateInfo(1).normalizedTime < 1.0f);
+
+        bodyAnim.SetTrigger("Angry");
+
+        yield return new WaitUntil(() => bodyAnim.GetCurrentAnimatorStateInfo(1).IsName("Molesto 1") &&
+    bodyAnim.GetCurrentAnimatorStateInfo(1).normalizedTime < 1.0f);
+
+        bodyAnim.SetBool("IsStress", false);
+        bodyAnim.SetBool("IsTalking", false);
+        tailAnim.SetBool("Angry", false);
+
+        yield return new WaitForSeconds(3f);
     }
 
     public void SetSpeakingBool(bool state)
     {
         Debug.Log("Speaking " + state);
-        anim.SetBool("IsTalking", state);
+        bodyAnim.SetBool("IsTalking", state);
     }
 
     public void SetMovingBool(bool state)
     {
-        anim.SetBool("IsMoving", state);
+        bodyAnim.SetBool("IsMoving", state);
     }
 
     public void SetIndex(int index)
     {
-        anim.SetInteger("Index", index);
+        bodyAnim.SetInteger("Index", index);
+    }
+    public void TailAngry(bool state)
+    {
+        tailAnim.SetBool("Angry", state);
+    }
+
+    public void TailHappy(bool state)
+    {
+        tailAnim.SetBool("Happy", state);
+    }
+    
+    public void TailSad(bool state)
+    {
+        tailAnim.SetBool("Sad", state);
     }
 
     public void SetBlinkBool(bool state)
     {
-        anim.SetBool("IsBlinking", state);
+        bodyAnim.SetBool("IsBlinking", state);
     }
 }
