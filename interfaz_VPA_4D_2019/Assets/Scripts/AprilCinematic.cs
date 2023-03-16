32323f72d90ae8b1153644e36b398327f89ba263
ProfDialfes <Dialfes@gmail.com>
2023-03-16 13:19:12 -0500

    Creacion de Signals y animator

---
+++ interfaz_VPA_4D_2019/Assets/Scripts/AprilCinematic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AprilCinematic : MonoBehaviour
+{
+    [SerializeField]
+    Animator aprilAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aprilAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    public void SetFly(bool state)
+    {
+        aprilAnim.SetBool("isFly", state);
+    }
+    public void SetIdle(bool state)
+    {
+        aprilAnim.SetBool("isIdle", state);
+    }
+    public void SetJump(bool state)
+    {
+        aprilAnim.SetBool("isFly", state);
+    }
+    public void SetGround(bool state)
+    {
+        aprilAnim.SetBool("isGround", state);
+    }
+}
